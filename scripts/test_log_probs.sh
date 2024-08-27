ee58c50840e4a5bc7e19c6f6a6c9deec1a14e3ab
Joshuaclymer <joshmclymer@gmail.com>
2024-08-26 21:08:48 -0700

    clean up

--- scripts/test_log_probs.sh
+++
@@ -1,3 +0,0 @@
-python src/api/evaluate_strategy.py \
-    --path_of_strategy src/detection_strategies/log_prob_strat.py \
-    --output_path results/log_probs.json
--- scripts/test_random_strategy.sh
+++
@@ -1,3 +0,0 @@
-python src/api/evaluate_strategy.py \
-    --path_of_strategy src/detection_strategies/random.py \
-    --output_path results/random.json
